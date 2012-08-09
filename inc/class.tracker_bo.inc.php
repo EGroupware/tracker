ef2aaf310ccea9365276626acc667a98c5932221
Ralf Becker <ralfbecker@outdoor-training.de>
2012-08-09 09:53:34 +0000

    avoid calling accounts->members(hips) without an account_id and cast accounts->members(hips) to array, to cope without PHP warnings for not (longer) existing accounts

--- inc/class.tracker_bo.inc.php
+++ inc/class.tracker_bo.inc.php
@@ -619,7 +619,7 @@ class tracker_bo extends tracker_so
 			if ($GLOBALS['egw']->accounts->get_type($uid) == 'g')
 			{
 				if ($return_groups) $groups[(string)$uid] = $GLOBALS['egw']->common->grab_owner_name($uid);
-				foreach($GLOBALS['egw']->accounts->members($uid,true) as $u)
+				foreach((array)$GLOBALS['egw']->accounts->members($uid,true) as $u)
 				{
 					if (!isset($users[$u])) $users[$u] = $GLOBALS['egw']->common->grab_owner_name($u);
 				}
@@ -779,8 +779,7 @@ class tracker_bo extends tracker_so
 				// group assinged
 				if ($this->allow_assign_groups && $assignee < 0)
 				{
-					$members = $GLOBALS['egw']->accounts->members($assignee,true);
-					if (in_array($this->user,$members)) return true;
+					if (($members = $GLOBALS['egw']->accounts->members($assignee,true)) && in_array($this->user,$members)) return true;
 				}
 			}
 		}
