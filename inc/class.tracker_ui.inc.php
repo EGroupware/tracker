f1629796302c47259d0d9161a8056f4c9ed10228
Nathan Gray <nathangray.bsc@gmail.com>
2013-09-23 15:57:25 +0000

    Fix tracker assigned to: none column filter

--- inc/class.tracker_ui.inc.php
+++ inc/class.tracker_ui.inc.php
@@ -745,10 +745,6 @@ class tracker_ui extends tracker_bo
 			$query['col_filter']['tr_assigned'] = $GLOBALS['egw']->accounts->members($query['col_filter']['tr_assigned'],true);
 			$query['col_filter']['tr_assigned'][] = $query_in['col_filter']['tr_assigned'];
 		}
-		elseif($query['col_filter']['tr_assigned'] === 'not')
-		{
-			$query['col_filter']['tr_assigned'] = null;
-		}
 		elseif(!$query['col_filter']['tr_assigned'])
 		{
 			unset($query['col_filter']['tr_assigned']);
@@ -817,6 +813,7 @@ class tracker_ui extends tracker_bo
 		if ($query['col_filter']['multi_queue']) unset($query['col_filter']['multi_queue']);
 
 		//echo "<p align=right>uitracker::get_rows() order='$query[order]', sort='$query[sort]', search='$query[search]', start=$query[start], num_rows=$query[num_rows], col_filter=".print_r($query['col_filter'],true)."</p>\n";
+	_debug_array($query['col_filter']);
 		$total = parent::get_rows($query,$rows,$readonlys,$this->allow_voting||$this->allow_bounties);	// true = count votes and/or bounties
 		foreach($rows as $n => $row)
 		{
