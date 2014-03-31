4e00871781d435eadef5dfc2c89b19cfa90b6638
Klaus Leithoff <kl@stylite.de>
2014-03-31 12:12:54 +0000

    try to handle PHP Warning: Illegal offset type in tracker ui for possible tracker specific template

--- inc/class.tracker_ui.inc.php
+++ inc/class.tracker_ui.inc.php
@@ -677,12 +677,12 @@ class tracker_ui extends tracker_bo
 		$content['no_links'] = $readonlys['link_to'];
 		$content['bounties']['no_set_bounties'] = $readonlys['bounty'];
 
-		$what = ($tracker && isset($this->trackers[$tracker]) ? $this->trackers[$tracker] : lang('Tracker'));
+		$what = ($tracker && isset($this->trackers[(is_array($tracker)?$tracker[0]:$tracker)]) ? $this->trackers[(is_array($tracker)?$tracker[0]:$tracker)] : lang('Tracker'));
 		$GLOBALS['egw_info']['flags']['app_header'] = $tr_id ? lang('Edit %1',$what) : lang('New %1',$what);
 
 		$tpl = new etemplate('tracker.edit');
 		// use a type-specific template (tracker.edit.xyz), if one exists, otherwise fall back to the generic one
-		if (!$tpl->read('tracker.edit.'.trim($this->trackers[$tracker])))
+		if (!$tpl->read('tracker.edit'.(isset($this->trackers[(is_array($tracker)?$tracker[0]:$tracker)])?'.'.trim($this->trackers[(is_array($tracker)?$tracker[0]:$tracker)]):'')))
 		{
 			$tpl->read('tracker.edit');
 		}
