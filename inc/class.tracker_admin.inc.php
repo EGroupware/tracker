f23410728771ff29ab0667d831b020c2a4d0a37e
Ralf Becker <RalfBecker@outdoor-training.de>
2016-05-31 20:35:18 +0200

    fix 16.1 Api\Translation wont work in 14.x

--- inc/class.tracker_admin.inc.php
+++ inc/class.tracker_admin.inc.php
@@ -539,7 +539,7 @@ class tracker_admin extends tracker_bo
 			'allow_bounties' => array('No','Yes'),
 			'autoassign' => $this->get_staff($tracker),
 			'lang' => ($tracker ? array('' => lang('default')) : array() )+
-				Api\Translation::get_installed_langs(),
+				translation::get_installed_langs(),
 			'cat_id' => $this->get_tracker_labels('cat',$tracker),
 			// Mail handling
 			'interval' => array(
