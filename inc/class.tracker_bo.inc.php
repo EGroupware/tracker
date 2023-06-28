7fea0626ca3076f65ccb53bd7fdbea660b85d82c
nathan <nathangray.bsc+github@gmail.com>
2023-06-28 11:58:53 -0600

    Tracker: Fix overall default queue was overriding mail import default queue

--- inc/class.tracker_bo.inc.php
+++ inc/class.tracker_bo.inc.php
@@ -2133,6 +2133,7 @@ class tracker_bo extends tracker_so
 			$trackerentry = $this->init(
 				array(
 					'tr_id'          => 0,
+					'tr_tracker'     => $this->mailhandling[$_queue]['default_tracker'],
 					'tr_cc'          => implode(',', $emails),
 					'tr_summary'     => $_subject,
 					'tr_description' => $_message,
